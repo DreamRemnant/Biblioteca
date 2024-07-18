f000715dff984de4815a4ba0d40336935f46309a
DreamRemnant <jfmg.64@gmail.com>
2024-07-17 22:15:49 -0700

    Agregar archivos de proyecto.

---
+++ Libro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    internal class Libro
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int ISBN { get; set; }
+        public bool Estado { get; set; }
+
+        public Libro(string Titulo, string Autor, int ISBN)
+        {
+            this.Titulo = Titulo;
+            this.Autor = Autor;
+            this.ISBN = ISBN;
+            Estado = false;
+        }
+    }
+}
