3ad110313bd1b30502da2321da5b1c1cae3839c1
DreamRemnant <jfmg.64@gmail.com>
2024-07-20 01:54:21 -0700

    Se creó la clase usuarios.

---
+++ Usuarios.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    internal class Usuarios
+    {
+        static List<Persona> Personas = new List<Persona>();
+
+        public Persona ObtenerPorUsername (string username)
+        {
+            foreach (Persona usuario in Personas)
+            {
+                if (usuario.Username == username)
+                {
+                    return usuario;
+                }
+            }
+
+            throw new Exception("No se encontró un usuario con ese Username.");
+        }
+
+        public Persona ObtenerPorNombre(string nombre)
+        {
+            foreach (Persona usuario in Usuarios)
+            {
+                if (usuario.Nombre == nombre)
+                {
+                    return usuario;
+                }
+            }
+
+            throw new Exception("No se encontró un usuario con ese Username.");
+        }
+    }
+}
